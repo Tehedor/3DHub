4f26afd8185b4127d994dab0d57a6715cd19a496
tehedor <sergiot1805@gmail.com>
2024-04-07 17:59:04 +0200

    Configuración de todaas las apis, pero no funcioann todavia

--- fronted/src/services/diseñador/pedidos.service.js
+++ fronted/src/services/diseñador/pedidos.service.js
@@ -1,60 +1,37 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/";
+// const API_URL = "http://localhost:8080/";
 
-const register = (username, email, password) => {
-  return axios.post(API_URL + "createUser", {
-    username,
-    email,
-    password,
-    roles: ["ROLE_DESIGNER"],
-  });
-};
+const user = JSON.parse(localStorage.getItem("user"));
 
-const login = (username, password) => {
-  return axios
-    .post(API_URL + "loginDesigner", {
-      username,
-      password,
+const token = user ? user.accessToken : "";
+
+const app = axios.create({
+  baseURL: "http://localhost:8080/",
+  headers: {
+    "Content-type": "application/json",
+    "Authorization": `Bearer ${token}`,
+  },
+});
+
+const revisado = (id) => {
+  return app
+    .post(`api/orders/${id}/status`, {
+      "name": "PAY"
     })
-    .then((response) => {
-      if (response.data.username) {
-        localStorage.setItem("user", JSON.stringify(response.data)); // localStorage.setItem("user", JSON.stringify(response.data));: Si la propiedad username existe, entonces se almacena el objeto data de la respuesta en el almacenamiento local del navegador bajo la clave "user". Antes de almacenarlo, el objeto data se convierte en una cadena JSON.
-      }
-
-      return response.data;
-    });
-};
-
-const logout = () => {
-  localStorage.removeItem("user");
-  return axios.post(API_URL + "signout").then((response) => {
-    return response.data;
-  });
-};
-
-const getCurrentUser = () => {
-  return JSON.parse(localStorage.getItem("user"));
-};
-
-const order = () => {
-  return axios
-    .post(API_URL + "order", {
-      headers: {
-        Authorization: `Bearer ${getCurrentUser().token}`,
-      },
-    })
-    .then((response) => {
-      return response.data;
-    });
 }
 
-const AuthService = {
-  register,
-  login,
-  logout,
-  getCurrentUser,
-  order,
+const confirmarEntrga = (id) => {
+  return app
+    .post(`api/orders/${id}/status`, {
+      "name": "DELIVERED"
+    })
 }
 
-export default AuthService;
+
+const PedidosService = {
+  revisado,
+  confirmarEntrga,
+}
+
+export default PedidosService;
