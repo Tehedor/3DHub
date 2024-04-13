888b9cd0800015b7f564e9ee89478c6f8bdde502
tehedor <sergiot1805@gmail.com>
2024-04-13 02:18:34 +0200

    nuevo ver pedir la tajeta de las impresoras

--- fronted/src/components/home/VerPedir.js
+++ fronted/src/components/home/VerPedir.js
@@ -1,67 +1,84 @@
 import {Container, Card,Row, Col, Button, Image} from "react-bootstrap";
-import {Link} from "react-router-dom";
+
+import StarRatings from 'react-star-ratings';
 
 export default function Ver(props) {
 
+    // ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+    // ##### ##### Variables descarga
+    // ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
     const printer = props.printer;
-
     const printerId = props.printer.id;
     
+    // Api change
+    const ratingProducto = 3.5;
+    
+    // ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+    // ##### ##### Return
+    // ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
     return(
         <Card border="gray" style={{ backgroundColor: "white", marginTop: '0' }}> 
-        {/* <Card border="gray" style={{ backgroundColor: "white", marginTop: '0', height: '320px' }}>  */}
-            <Card.Body>
-
-            <Row>
-                <Col sm={3} class="imagen" className="d-flex justify-content-center align-items-center">
-                    {/* <Image src={printer.Foto_impresora} thumbnail  style={{ maxWidth: "100%" }}/> */}
-                    <Card.Img src={printer.printerPhoto || 'https://m.media-amazon.com/images/I/61L4aoIqYOL._AC_SX466_.jpg'} style={{ maxWidth: "100%" }}/>  
-                </Col>
-                <Col sm={9} class="datos_impresora">
-                    <Row >
-                        <Col sm={4}>
+            <Card.Body style={{ paddingTop: 0, paddingBottom: 0 }}>
+                <Row>
+                    <Col md={6} lg={3} class="imagen" className="d-flex justify-content-center align-items-center">
+                    <Container style={{ width: "100%", height: "auto", overflow: "hidden", boxShadow: "0px 0px 10px 2px rgba(0,0,0,0.1)" }}>
+                        <Image
+                        src={printer.printerPhoto || "http://localhost:3000/printer_default.jpg"}
+                        style={{ width: "100%", objectFit: "cover" }}
+                        thumbnail
+                        />
+                    </Container>
+                    </Col>
+                    <Col md={6} lg={9} class="datos_impresora">
+                        <Row style={{backgroundColor: "gray"}}>
+                       <Col sm={4} style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                             <Card.Text style={{color: 'black'}}>Nombre: {printer.modelName}</Card.Text>
                         </Col>
-                        <Col sm={4}>
-                            <Row noGutters>
-                                <Container>
-                                 <Image src="http://localhost:3000/iconos/star_fill_icon.svg" height="20px" width="20px"/>
-                                 <Image src="http://localhost:3000/iconos/star_fill_icon.svg" height="20px" width="20px"/>
-                                 <Image src="http://localhost:3000/iconos/star_fill_icon.svg" height="20px" width="20px"/>
-                                 <Image src="http://localhost:3000/iconos/star_fill_icon.svg" height="20px" width="20px"/>
-                                 <Image src="http://localhost:3000/iconos/star_fill_icon.svg" height="20px" width="20px"/>
-                                </Container>
-                            </Row>
+                            <Col sm={4}>
+                                <Row noGutters>
+                                    <Container>
+                                    <StarRatings
+                                        // rating={props.ratingProducto}
+                                        rating={ratingProducto}
+                                        starRatedColor="orange"
+                                        numberOfStars={5}
+                                        name='ratingProducto'
+                                        starDimension="20px"
+                                        starSpacing="0px"
+                                        
+                                    />
+                                    </Container>
+                                </Row>
+                                
+                            </Col>
+                            <Col sm={4} style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                <Card.Text>Fabricante: <strong>{printer.userIdFabricante}</strong>, Id_imprsora: {printer.id}</Card.Text>
+                            </Col>
                             
-                        </Col>
-                        <Col sm={4}>
-                            <Card.Text>Fabricante: {printer.userIdFabricante}</Card.Text>
-                            <Card.Text>Id: {printer.id}</Card.Text>
-                        </Col>
-                        
 
-                    </Row>
-                    <Row>
-                        <Col sm={6}>
-                            <Card.Text>Tipo: {printer.printerType}</Card.Text>
-                            <Card.Text>Max_unid: {printer.maxUnities}</Card.Text>
-                            <Card.Text>Velocidad: {printer.manufacturationSpeed}</Card.Text>
-                            <Card.Text>Max_ancho: {printer.maxWidth}</Card.Text>
-                            <Card.Text>Max_alto: {printer.maxHeight}</Card.Text>
-                        
-                        </Col>
-                        <Col sm={6}>
-                            <Card.Text>Precisión: {printer.printerPrecision}</Card.Text>
-                            <Card.Text>Colores: {printer.color}</Card.Text>
-                            <Card.Text>Material: {printer.material}</Card.Text>
-                            {/* <Card.Text>Acabados: {printer.material}</Card.Text> */}
-                            <Card.Text>Precio: {printer.servicePrice} €/mm³/</Card.Text>
+                        </Row>
+                        <Row style={{marginTop: "10px"}}>
+                            <Col sm={6}>
+                                <Card.Text>Tipo: {printer.printerType}</Card.Text>
+                                <Card.Text>Max_unid: {printer.maxUnities}</Card.Text>
+                                <Card.Text>Velocidad: {printer.manufacturationSpeed}</Card.Text>
+                                <Card.Text>Max_ancho: {printer.maxWidth}</Card.Text>
+                                <Card.Text>Max_alto: {printer.maxHeight}</Card.Text>
+                            
+                            </Col>
+                            <Col sm={6}>
+                                <Card.Text>Precisión: {printer.printerPrecision}</Card.Text>
+                                <Card.Text>Colores: {printer.color}</Card.Text>
+                                <Card.Text>Material: {printer.material}</Card.Text>
+                                {/* <Card.Text>Acabados: {printer.material}</Card.Text> */}
+                                <Card.Text>Precio: {printer.servicePrice} €/mm³/</Card.Text>
 
-                        </Col>
-                    </Row>
-                    {/* <Card.Text>Stock: {printer.Nombre_modelo}</Card.Text> */}
-                </Col> 
-            </Row>
+                            </Col>
+                        </Row>
+                        {/* <Card.Text>Stock: {printer.Nombre_modelo}</Card.Text> */}
+                    </Col> 
+                </Row>
+
             </Card.Body>
         </Card>  
 
