3bbcfab50ea3a2dbc8e138011007af5d479961c0
Denzel1604 <denzel.salcab@gmail.com>
2024-05-05 20:23:03 +0200

    API Ubicacion

--- backend/src/main/java/com/dhub/backend/services/SearchServiceImpl.java
+++ backend/src/main/java/com/dhub/backend/services/SearchServiceImpl.java
@@ -4,13 +4,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import com.dhub.backend.models.EPrinterType;
 import com.dhub.backend.models.Printer;
 import com.dhub.backend.repository.PrinterRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.annotation.PostConstruct;
 
@@ -18,103 +23,76 @@ public class SearchServiceImpl {
 
     public static void main(String[] args) {
         SearchServiceImpl service = new SearchServiceImpl();
-        service.initPrinters();
-        System.out.println(service.closestPrinter("Calle Miami, 1, Madrid, España"));
+        // System.out.println(service.coordinates("Calle de la Montera, 32, 28013 Madrid, España"));
+        String origen = service.coordinates("Calle de la Montera, 32, 28013 Madrid, España");
+        String destino = service.coordinates("Camp Nou, Barcelona, España");
+        String duracion = getDuration(origen,destino);
+        System.out.println((duracion));
     }
-    @Autowired
-    private PrinterRepository printerRepository;
-    String [] locations = {"Calle de la Princesa, 1, Madrid, España", "Calle de Alcalá, 230, Madrid, España", "Estadio Santiago Bernabéu, Madrid, España"};
-
-    @PostConstruct
-    public void initPrinters() {
-        for (int i = 0; i < locations.length; i++) {
-            Printer printer = new Printer();
-            printer.setModelName("Model Name");
-            printer.setPrinterLocation(locations[i]);
-            printer.setPrinterType(EPrinterType.FDM); // replace with actual type
-            printer.setFileFormat("File Format");
-            printer.setPrinterPhoto(new byte[0]); // replace with actual photo
-            printer.setServicePrice(0.0);
-            printer.setMaxUnities(0);
-            printer.setManufacturationSpeed("Manufacturation Speed");
-            printer.setMaxWidth(0.0);
-            printerRepository.save(printer);
-        }
-    }
-
-    public Long closestPrinter(String locUser) {
-        String userLoc = coordinates(locUser);
-        List<Printer> printers = printerRepository.findAll();
-
-        double minDistance = Double.MAX_VALUE;
-        Printer closestPrinter = printers.get(0);
-        for (Printer printer : printers) {
-            String printerLoc = coordinates(printer.getPrinterLocation());
-            double distance = distance(userLoc, printerLoc);
-            if (distance < minDistance) {
-                minDistance = distance;
-                closestPrinter = printer;
-            }
-        }
-        return closestPrinter.getId();
-    }
-
-    public List<Printer> closestPrinters(String locUser) {
-        List<Printer> printers = printerRepository.findAll();
-        for (int i = 0; i < printers.size(); i++) {
-            for (int j = 0; j < printers.size(); j++) {
-                String printerLocI = coordinates(printers.get(i).getPrinterLocation());
-                String printerLocJ = coordinates(printers.get(j).getPrinterLocation());
-                if (distance(locUser, printerLocI) < distance(locUser, printerLocJ)) {
-                    Printer temp = printers.get(i);
-                    printers.set(i, printers.get(j));
-                    printers.set(j, temp);
-                }
-            }
-        }
-        return printers;
-    }
-
-    private double distance(String locUser, String printerLocation) {
-        final double R = 6371000;
-        double theta1 = Math.toRadians(Double.parseDouble(locUser.split(",")[0]));
-        double theta2 = Math.toRadians(Double.parseDouble(printerLocation.split(",")[0]));
-        double phy1 = Math.toRadians(Double.parseDouble(locUser.split(",")[1]));
-        double phy2 = Math.toRadians(Double.parseDouble(printerLocation.split(",")[1]));
-        double deltaTheta = theta2 - theta1;
-        double deltaPhy = phy2 - phy1;
-        double distance = 2 * R * Math.asin(Math.sqrt(Math.pow(Math.sin(deltaTheta / 2),2) + Math.cos(theta1) * Math.cos(theta2) * Math.pow(Math.sin(deltaPhy / 2),2)));
-
-        return distance;
-    }
+    // @Autowired
+    // private PrinterRepository printerRepository;
 
     /*
      * Get coordinates from a location inputed as String
      */
     private String coordinates(String location) {
-        URL pythonScriptUrl = getClass().getResource("/python/position.py");
-        String pythonScriptPath = pythonScriptUrl.getPath();
+        // Ruta al script de Python
+        String pythonScriptPath = "C:\\Users\\denze\\Documents\\GitHub\\3DHub\\backend\\src\\main\\resources\\python\\position.py"; // Actualiza la ruta según la ubicación de tu script
+
+        // Comando para ejecutar el script de Python
         String[] cmd = new String[3];
-        cmd[0] = "python3";
+        cmd[0] = "python"; // Ejecuta el comando "python" en lugar de "python3" en Windows
         cmd[1] = pythonScriptPath;
         cmd[2] = location;
 
         String output = "";
 
         try {
+            // Crear el proceso para ejecutar el script de Python
             ProcessBuilder pb = new ProcessBuilder(cmd);
             Process process = pb.start();
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            // Leer la salida del proceso
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line = null;
-            while ((line = in.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 output += line;
             }
-            in.close();
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return output;
+    }
+
+    public static String getDuration(String origen, String destino){
+                // Construye la URL con los parámetros recibidos
+        String apiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json" +
+                "?origins=" + origen +
+                "&destinations=" + destino +
+                "&units=" + "decimal" +
+                "&key=" + "AIzaSyBpo1XjPia4u_NUHO60g8kILg9vgZb9AP0";
+
+        // Realiza la solicitud GET usando RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
+        // Parsea la respuesta JSON
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root = mapper.readTree(response.getBody());
+            JsonNode rowsNode = root.get("rows");
+            JsonNode elementsNode = rowsNode.get(0).get("elements");
+            JsonNode durationNode = elementsNode.get(0).get("duration");
+            String durationText = durationNode.get("text").asText();
+            
+            // Devuelve el texto de la duración
+            return durationText;
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Maneja el error de alguna manera adecuada, por ejemplo, lanzando una excepción personalizada
+            throw new RuntimeException("Error al procesar la respuesta JSON");
+        }
     }
 }
