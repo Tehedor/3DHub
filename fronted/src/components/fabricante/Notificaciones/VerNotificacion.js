a33be18a94c70c138042ac200b8a7a6ee6dbf404
tehedor <sergiot1805@gmail.com>
2024-05-06 14:20:00 +0200

    cambiar cantidad

--- fronted/src/components/fabricante/Notificaciones/VerNotificacion.js
+++ fronted/src/components/fabricante/Notificaciones/VerNotificacion.js
@@ -151,7 +151,7 @@ export default function VerNotificaciones(props) {
                                 </tr>
                                 <tr>
                                     <td style={{ paddingLeft: '0px', textAlign: 'left', width: '57%' }}>Cantidad:</td>
-                                    <td style={{ textAlign: 'left' }}>{pedidos.quantity.toFixed(2)} </td>
+                                    <td style={{ textAlign: 'left' }}>{pedidos.quantity} </td>
                                 </tr>
                                 <tr>
                                     <td style={{ paddingLeft: '0px', textAlign: 'left', width: '57%' }}><strong>P. Total:</strong></td>
